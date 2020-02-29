eb5c7d9f701663b17c79430eb373eb96ad961571
rederrorsuru <rederrorsuru@gmail.com>
2020-02-29 19:27:07 +0100

    Added missing files

---
+++ bg_col outline.sh
@@ -0,0 +1,12 @@
+THISFILE=`basename "$0"`
+PYSCRIPTPATH="${0%\\*}\\change_background.py" #if the .sh script is somewhere else, put the absolute path instead #${0%\\*}\\change_background.py"
+#cd "${0%\\*}"
+echo ALL : $@
+PARAM_FOLDER="${1%\\*}" #The python script will be local to the image (for creating new ones)
+cd "$PARAM_FOLDER"
+(
+bash "${0%\\*}\\_unoptimize_gif.sh" "$@"
+echo python "$PYSCRIPTPATH" --outline "$@"
+python "$PYSCRIPTPATH" --outline "$@"
+)
+exec $SHELL
