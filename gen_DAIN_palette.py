c92de79c4b9971b31f59e69253f59cf933388ec9
rederrorsuru <rederrorsuru@gmail.com>
2020-02-29 21:31:11 +0100

    Scipt to directly look for the 'interpolated_frames' folder

---
+++ gen_DAIN_palette.py
@@ -0,0 +1,17 @@
+import os.path
+import os
+try:
+    if __name__ == "__main__":
+        import sys
+        if(len(sys.argv)>1):
+            for arg in sys.argv[1:]:
+                name,extension = os.path.splitext(arg)
+                file = open(name+"_palette.sh","w")
+                file.write('cd "${0%\\\\*}"' +"""
+(
+python \""""+os.getcwd()+os.sep+"""transfer_palette.py" "%s" "$1""\\interpolated_frames" ;
+) || read  -n 1 -p "Failed. Press enter." mainmenuinput ;"""%arg)
+        file.close()
+except Exception as e:
+    input("Failure\n"+str(e))
+    raise(e)
