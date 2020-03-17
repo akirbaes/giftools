603620e33541126b7b97ca94ea4385e9e976fef9
akirbaes <akirbaes@ulb.ac.be>
2020-03-17 22:12:01 +0100

    Both directions now use the same function so that there are less modifications to do when fixing stuff. Also fixed stuff.

--- STACK_gifs.py
+++ STACK_gifs.py
@@ -1,7 +1,8 @@
 from PIL import Image
 import os.path
 import os
-BGCOLOR = (255,255,255)
+from transfer_palette import index_rgb_alpha
+BGCOLOR = (50,50,50,0)
 HOR = 0
 VER = 1
 """def load_palette(filename):
@@ -26,6 +27,88 @@ def compute_lcm(x, y):
 def index_image(image):
     return image.quantize(colors=256, method=None, kmeans=0, dither=0)
     
+def stack_gifs_vh(filenames,direction=HOR,centering="center"):
+    print(filenames)
+    images = list()
+    width = 0
+    height = 0
+    lengths = list()
+    total_length = 1
+    total_duration = 0
+    hor,ver = direction is HOR, direction is VER
+    
+    if(hor):    xs = list()
+    if(ver):    ys = list()
+    #For starters, only lengths TOTAL or 1 are accepted
+    for filename in filenames:
+        im = Image.open(filename)
+        images.append(im)
+        if(hor):
+            xs.append(width)
+            width+=im.width
+            height=max(height,im.height)
+        if(ver):
+            ys.append(height)
+            height+=im.height
+            width=max(width,im.width)
+        try:
+            lengths.append(im.n_frames)
+            total_length=max(total_length,im.n_frames)
+            total_duration = max(im.n_frames*im.info['duration'],total_duration)
+        except:
+            lengths.append(1)
+    
+    print("Image",width,"x",height)
+    results = list()
+    print(lengths)
+    
+    for t in range(total_length):
+        target =  Image.new('RGBA', (width, height), BGCOLOR)
+        results.append(target)
+        
+    for i,img in enumerate(images):
+        length = lengths[i]
+        if(hor):
+            x=xs[i]
+            if(centering=="top"):       y=0
+            elif(centering=="bottom"):  y=height-img.height
+            else:                       y=int((height-img.height)/2)
+        if(ver):
+            y=ys[i]
+            if(centering=="top"):       x=0
+            elif(centering=="bottom"):  x=width-img.width
+            else:                       x=int((width-img.width)/2)
+        
+        if(length==1):
+            for target in results:
+                target.paste(img,(x,y))
+        else:
+            ratio = int(total_length/length)
+            print("Anim of length",length,"of ratio",ratio)
+            for j in range(length):
+                for k in range(ratio):
+                    print("Image",i,"frame",j,"ratio",k)
+                    results[j*ratio+k].paste(img,(x,y))
+                try:
+                    img.seek(img.tell()+1)
+                except:
+                    pass
+    
+    for t in range(len(results)):
+        results[t]=index_rgb_alpha(results[t])
+        #index_image(results[t])
+        
+    #results[0].convert("P").show()
+    folder = os.path.dirname(filenames[0]).strip()
+    input(folder)
+    names = "".join([os.path.splitext(os.path.basename(f))[0] for f in filenames])
+    if(folder):
+        outputname = os.sep.join((folder,names+".gif"))
+    else:
+        outputname = names+".gif"
+    print("Exporting",outputname)
+    results[0].save(outputname, "GIF", save_all=True,append_images=results[1:], optimize=False, disposal=2, duration=total_duration/total_length, loop=0, transparency=0)
+        
 def stack_gifs_hor(filenames,centering="center"):
     print(filenames)
     images = list()
@@ -106,8 +189,8 @@ def stack_gifs_ver(filenames,centering):
         width=max(width,im.width)
         try:
             lengths.append(im.n_frames)
-            total_length=im.n_frames
-            total_duration = im.n_frames*im.info['duration']
+            total_length=max(total_length,im.n_frames)
+            total_duration = max(im.n_frames*im.info['duration'],total_duration)
         except:
             lengths.append(1)
     
@@ -130,8 +213,12 @@ def stack_gifs_ver(filenames,centering):
             for target in results:
                 target.paste(img,(x,y))
         else:
+            ratio = int(total_length/length)
+            print("Anim of length",length,"of ratio",ratio)
             for j in range(length):
-                results[j].paste(img,(x,y))
+                for k in range(ratio):
+                    print("Image",i,"frame",j,"ratio",k)
+                    results[j*ratio+k].paste(img,(x,y))
                 try:
                     img.seek(img.tell()+1)
                 except:
@@ -173,11 +260,9 @@ try:
             file_list = list()
             for arg in sys.argv[1:]:
                 file_list.append(arg)
-                
-            if(direction==HOR):
-                stack_gifs_hor(file_list,centering)
-            else:
-                stack_gifs_ver(file_list,centering)
+            
+            
+            stack_gifs_vh(file_list,direction,centering)
             #pal = load_palette(source)
             """pal = Image.open(source)
             
