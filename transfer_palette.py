8a1b10461bd47f8142e9df3615989d4c51ca5973
akirbaes <akirbaes@ulb.ac.be>
2020-03-29 16:12:46 +0200

    Making the over50fps fix work

--- transfer_palette.py
+++ transfer_palette.py
@@ -13,7 +13,10 @@ from statistics import mode
     
 from gif_manips import remove_unused_color_from_palette, index_rgb_and_alpha
 
-def create_gif_from_folder(foldername,outputname=None,palette=None,allow_dropped_frames=True):
+def create_gif_from_folder(foldername,outputname=None,palette=None,default_duration=20,allow_dropped_frames=2):
+    #Default duration for when there is not actually a duration name in the file
+    #Allow_dropped_frames=1/True for when frames are 10ms (remove one out of two 10ms frames)
+    #Allow_dropped_frames=2 to sacrifice as many frames as there is missing time (when interpolated too much)
     images = list()
     durations = list()
     previous_time = 0
@@ -21,45 +24,64 @@ def create_gif_from_folder(foldername,outputname=None,palette=None,allow_dropped
         palette=remove_unused_color_from_palette(palette) #this actually ends up mangling the colors, so only do it once
         #[TODO] Look at the colors of all the frames and not only frame 1
     borrowed_time = 0
+    total=0
+    dropped = 0
+    added = 0
     for file in os.listdir(foldername):
         if file.endswith(".png") or file.endswith(".gif"):
+            total+=1
             im = Image.open(foldername+os.sep+file)
             
-            try:
-            
+            try: #Load durations from timestamps
                 time = int(file.split(".")[-2])
-                if(time!=0):
-                    
+                if(time!=0):    #Not the first frame 
                     duration = time-previous_time
-                    if(allow_dropped_frames):
-                        duration-=borrowed_time
-                        if(duration==0):
-                            borrowed_time=0
-                            continue
-                        elif(duration==10):
-                            borrowed_time = 20-duration
-                    else:
-                        if(duration==10):
-                            duration=20
-                    durations.append(duration)
                     previous_time = time
+                    if(duration==1):
+                        #Mode 1 or 2, not an actual duration
+                        duration = default_duration
+                    elif(allow_dropped_frames):
+                        #Try to deal with framerate>50 by dropping frames
+                        duration-=borrowed_time
+                        if(duration<=0): #As of now, don't sacrifice more than one frame (I suppose no input over 100fps)
+                            if(allow_dropped_frames>1):
+                                borrowed_time=-duration #Still longing
+                                print(borrowed_time)
+                            else:
+                                borrowed_time=0 #Sacrifice accepted
+                            dropped+=1
+                            continue        #A real gestion of that would evenly space out the kept frames
+                        elif(duration<20):
+                            #I suppose borrow at most 10
+                            borrowed_time = 20-duration
+                            duration=20
+                        else:
+                            borrowed_time=0 #No issue
+                    else:
+                        #Force to 50fps
+                        duration=max(20,duration)
             except Exception as e:
-                print(e)
+                duration = default_duration
+                
+            durations.append(duration)
             images.append(index_rgb_and_alpha(im,palette,0))
+            added+=1
+            # images[-1].show()
             #Puts the transparent color at index 255 so that I can simply pass 255 as transparency
             #It seems PIL reserves 255 for transparency anyway
             #Edit: but then optimize=True will make it so that there are less than 128 colors sometimes... so 0 is a safer bet
-    
-    durations+=[20]*(len(images)-len(durations))
+    print("Drop",dropped,"Add",added,"Total",total)
+    durations+=[20]*(len(images)-len(durations)) #Fill the missing durations. Might get fixed if not using timestamps but actual values...
     #durations[-1]=1600
-    for im in images:
-        try:
-            del im.info['transparency']
-        except: pass
+    # for im in images:
+        # try:
+            # del im.info['transparency']
+        # except: pass
     print(len(images),len(durations))
     print(durations)
     #optimize=True will only try to reduce the size of the palette
-    images[0].save(outputname, "GIF", save_all=True,append_images=images[1:], optimize=True, tranparency=0,disposal=2, duration=durations, loop=0) 
+    images[0].save(outputname, "GIF", save_all=True,append_images=images[1:], optimize=True,disposal=2, duration=durations, loop=0) 
+
 
 def create_gif_from_image(filename,outputname=None,palette=None):
     im = Image.open(filename)
